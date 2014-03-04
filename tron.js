fd4ae07cbee3cf67f6cd01200135086f5385bb6a
nathanspeller <speller.nathan@gmail.com>
2014-03-04 15:55:00 -0800

    switched path class for a bike class

--- tron.js
+++ tron.js
@@ -15,27 +15,27 @@ $(function(){
         //get rid of this part: placeholder tron paths BEGIN =============================
         if(i === 34 && j >= 11){
           if (j != 11){
-          grid.append('<div class="cell tron1 path"></div>');
+          grid.append('<div class="cell tron1"></div>');
           } else {
-            grid.append('<div class="cell tron1"></div>');
+            grid.append('<div class="cell tron1 bike"></div>');
           }
         }else if(i === 4 && j <= 32){
           if (j != 32){
-          grid.append('<div class="cell tron2 path"></div>');
+          grid.append('<div class="cell tron2"></div>');
           } else {
-            grid.append('<div class="cell tron2"></div>');
+            grid.append('<div class="cell tron2 bike"></div>');
           }
         }else if(i === 9 && j >= 3){
           if (j != 3){
-          grid.append('<div class="cell tron3 path"></div>');
+          grid.append('<div class="cell tron3"></div>');
           } else {
-            grid.append('<div class="cell tron3"></div>');
+            grid.append('<div class="cell tron3 bike"></div>');
           }
         }else if(i === 20 && j <=28){
           if (j != 28){
-          grid.append('<div class="cell tron4 path"></div>');
+          grid.append('<div class="cell tron4"></div>');
           } else {
-            grid.append('<div class="cell tron4"></div>');
+            grid.append('<div class="cell tron4 bike"></div>');
           }
         } else {
         // END =============================================================================
